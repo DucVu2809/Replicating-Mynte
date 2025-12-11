ff6c226c3001b429b19c79a7b701c723f6e54b40
DucVu2809 <anhduc280905@gmail.com>
2025-12-12 04:18:15 +0700

    up khung xương của web

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log("Mynte Project Loaded Successfully!");
+
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('shadow-sm');
+    } else {
+        navbar.classList.remove('shadow-sm');
+    }
+});
